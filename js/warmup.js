be252a897a4a25816f10af5d980a3a8b1cf9c482
Rod-S <rodsanati@gmail.com>
2020-09-28 09:34:32 -0500

    created warmup files

---
+++ js/warmup.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log("Hello Vivian");
+console.log("Hello Sophie");
+console.log("Hello Samuel");
+console.log("Hello Brance");
+console.log("Hello Rod");
+
+function sayHello(name) {
+    console.log("Hello " + name);
+    //we will talk about returning later!
+}
+
+sayHello("Vivian");
+sayHello("Sophie");
