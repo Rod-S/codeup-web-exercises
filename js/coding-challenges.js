a4eef8dfb4ae9f1e398fd149ef4ed133d781b7bd
Rod-S <rodsanati@gmail.com>
2020-10-01 22:16:46 -0500

    remove console logs/debugger from narcissism function

--- js/coding-challenges.js
+++ js/coding-challenges.js
@@ -1,7 +1,6 @@
 //Narcissistic Numbers
 function narcissism() {
     var narcArray = [];
-    debugger
     for (let n=0; narcArray.length < 25; ++n) {
         var nDigitArray = n.toString().split("");
         var singleDigitRaisedArray = [];
@@ -10,18 +9,15 @@ function narcissism() {
         nDigitArray.forEach((nDigit) => {
             singleDigitRaisedArray.push(Math.pow(Number(nDigit), m));
             });
-        //console.log(singleDigitRaisedArray);
         if (singleDigitRaisedArray.length > 1) {
            combinedDigits = singleDigitRaisedArray.reduce((a, b) => a + b, 0)
         } else {
            combinedDigits = singleDigitRaisedArray[0];
         }
-        //console.log(combinedDigits);
         if (combinedDigits == n) {
             narcArray.push(n);
             document.querySelector(".narcNumber").innerHTML = narcArray;
         }
     }
-    console.log(narcArray);
 }
 narcissism();
